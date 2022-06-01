785a0bac473704858ef783999603f47123a773f8
Arif <arifajif@gmail.com>
2022-06-01 22:04:57 +0700

    tugas 2

---
+++ tugas2.js
@@ -0,0 +1,21 @@
+var nama = "arif";
+var umur = 24;
+
+var kg = 53;
+if (kg>55)
+berat = true;
+else 
+berat = false;
+
+var uang = "ada"
+uang = null;
+
+var provinsi=["jateng","jatim","jabar","yogya"]
+
+console.log(nama)
+console.log(umur)
+console.log(berat)
+console.log(uang)
+console.log(provinsi[0])
+
+
